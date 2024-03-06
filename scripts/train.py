49a9fea7f78109c45e1ff9977194e6ca8165804b
Klemens Iten <klemens.iten@hotmail.ch>
2024-03-06 13:06:36 +0100

    [task_4] implemented optuna

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 import wandb
+import optuna
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from plr_exercise.models.cnn import Net
@@ -55,6 +57,8 @@ def test(model, device, test_loader, epoch):
             test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
         )
     )
+    return test_loss
+
 
 
 def main():
@@ -106,42 +110,58 @@ def main():
     )
     parser.add_argument("--save-model", action="store_true", default=False, help="For Saving the current Model")
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    torch.manual_seed(args.seed)
+        # Optuna setup
+    study = optuna.create_study(direction='minimize')
+    def objective(trial):
+        lr = trial.suggest_loguniform('lr', 1e-5, 1e-1)
+        epochs = trial.suggest_int('epochs', 1, 20)
+        use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    if use_cuda:
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
+        torch.manual_seed(args.seed)
 
-    train_kwargs = {"batch_size": args.batch_size}
-    test_kwargs = {"batch_size": args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
+        if use_cuda:
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
 
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
-    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+        train_kwargs = {"batch_size": args.batch_size}
+        test_kwargs = {"batch_size": args.test_batch_size}
+        if use_cuda:
+            cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
+            train_kwargs.update(cuda_kwargs)
+            test_kwargs.update(cuda_kwargs)
 
-    model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
+        dataset2 = datasets.MNIST("../data", train=False, transform=transform)
+        train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+        test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(args.epochs):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader, epoch)
-        scheduler.step()
+        model = Net().to(device)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        
+        for epoch in range(1, epochs + 1):
+            train(args, model, device, train_loader, optimizer, epoch)
+            test_loss = test(model, device, test_loader, epoch)  
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        """
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+        for epoch in range(args.epochs):
+            train(args, model, device, train_loader, optimizer, epoch)
+            test(model, device, test_loader, epoch)
+            scheduler.step()
+        """
+
+        if args.save_model:
+            torch.save(model.state_dict(), "mnist_cnn.pt")
+        return test_loss
+    
+    study.optimize(objective, n_trials=100)
+    print("Best hyperparameters: ", study.best_params)
+
 
     run.finish()
-
 
 if __name__ == "__main__":
     main()
